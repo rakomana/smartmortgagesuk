8991e3c11cccc9c5aabe36f5b3b072e46eb02cff
Prince <princerakomana@gmail.com>
2020-10-14 15:03:23 +0200

    add team members

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -213,68 +213,94 @@
 		</div>
 		<!-- services-area-end -->
 		
-		<!-- clients-say-area-start -->
-		<div id="implement" style="padding-top: 50px;" class="clients-say-area  gray-bg">
-			<div class="row no-gutters">
-				<div class="col-xl-6 col-lg-6">
-					<div class="request-wrapper requests-wrapper bg-black">
-						<div class="request-text">
-							<h1>Take advantage of our mortgage calculator and unlock a better deal today!</h1>
-						</div>
-
-						<form @submit.prevent="sendMessage" id="request-form">
-							<div class="row">
-								<div class="col-xl-12 mb-15">
-									<input v-model="principal" name="principal" placeholder="How much do you want to borrow?" v-currency="{currency: 'EUR', locale: 'en'}" id="formattedNumberField"  type="text">
-								</div>
-								<div class="col-xl-12 mb-15">
-									<input v-model="years" name="years" placeholder="How many years" type="text">
-								</div>
-								<div class="col-xl-12 mb-15">
-									<input v-model="interest" name="interest" placeholder="Interest rate" type="number" step="1.0" min="0.1">
+		<!-- team-area-start -->
+		<div class="team-area gray-bg pt-120 pb-60">
+			<div class="container">
+				<div class="section-title text-center mb-50">
+					<h1>Meet the team</h1>
+				</div>
+				<div class="row">
+					<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+						<div class="team-wrapper team2-wrapper mb-55">
+							<div class="team2-img">
+								<img src="img/user.jpg" alt="" />
+								<div class="team2-text">
+									<ul class="team3-icon">
+										<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+										<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+										<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+									</ul>
 								</div>
 							</div>
-						</form>
-						<div v-if="interest && principal && years" class="clients-say-title">
-							<h1 style="color: white;">Results</h1>
-							<p>Your monthly payment will be: <span style="font-size: 18px; color: white;"><b>£ @{{remortgageCalculator}}</b></span></p>
-							<p>The total amount you will pay over the term is: <span style="font-size: 18px; color: white;"><b>£ @{{totalAmount}}</b></span></p>
+							<div class="team3-text">
+								<h4>Emma S. Shin</h4>
+								<span>Financial Advisor</span>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-xl-6 col-lg-6">
-					<div class="clients-say-wrapper" style="text-align: center;">
-						<div class="clienyts-say-active owl-carousel">
-							<div class="single-clients">
-								<div class="clients-say-text">
-									<h4>Mortgages</h4>
-									<p>Remortgage your house to free up capital to make those home improvements. You could also reduce your monthly payments through a lower rate. </p>
+					<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+						<div class="team-wrapper team2-wrapper mb-55">
+							<div class="team2-img">
+								<img src="img/user.jpg" alt="" />
+								<div class="team2-text">
+									<ul class="team3-icon">
+										<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+										<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+										<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+									</ul>
 								</div>
 							</div>
-							<div class="single-clients">
-								<div class="clients-say-text">
-									<h4>Secured Loans</h4>
-									<p>Loan is a highly popular and secure way to finance your home and by ‘secured’ it means you need to keep up with your repayments. </p>
+							<div class="team3-text">
+								<h4>Louis B. Thomas</h4>
+								<span>Banking Advisor</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+						<div class="team-wrapper team2-wrapper mb-55">
+							<div class="team2-img">
+								<img src="img/user.jpg" alt="" />
+								<div class="team2-text">
+									<ul class="team3-icon">
+										<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+										<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+										<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+									</ul>
 								</div>
 							</div>
-							<div class="single-clients">
-								<div class="clients-say-text">
-									<h4>Insurance</h4>
-									<p>Life insurance protect family, home insurance to protects home and valuables, conveyancing expert help </p>
+							<div class="team3-text">
+								<h4>Cassidy T. Ingham</h4>
+								<span>Business Advisor</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+						<div class="team-wrapper team2-wrapper mb-55">
+							<div class="team2-img">
+								<img src="img/user.jpg" alt="" />
+								<div class="team2-text">
+									<ul class="team3-icon">
+										<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+										<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+										<li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+									</ul>
 								</div>
 							</div>
-						</div><br>
-						<form method="post" action="{{url('/get-a-quote')}}">
-							@csrf
-							<input type="hidden" name="type" value="all">
-							<button class="btn btn-black" type="submit" data-animation="fadeInLeft" data-delay="1.2s">get my quote
-							</button>
-						</form>
+							<div class="team3-text">
+								<h4>Jerome J. Kondo</h4>
+								<span>Financial Advisor</span>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- clients-say-area-end -->		
+		<!-- team-area-end -->	
+
 
 	<script>
     const App = new Vue({
